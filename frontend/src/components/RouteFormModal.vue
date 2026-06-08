48d361f0c35c7d400268442456005f664fe659a8
qcg <qcg@panshi.local>
2026-06-08 22:05:05 +0800

    chore: 清理调试日志

--- frontend/src/components/RouteFormModal.vue
+++ frontend/src/components/RouteFormModal.vue
@@ -63,10 +63,8 @@
           <div class="form-row">
             <div class="form-group">
               <label class="form-label">上游 <span class="required">*</span></label>
-              <span style="display:none">{{ dbgLog('upstreams:', upstreams, 'length:', upstreams?.length) }}</span>
               <select v-model="form.upstream_id" class="form-input">
                 <option value="">请选择上游</option>
-                <option value="test">TEST硬编码</option>
                 <option v-for="u in upstreams" :key="u.id" :value="u.id">{{ u.name }}</option>
               </select>
               <div v-if="formErrors.upstream_id" class="form-error">{{ formErrors.upstream_id }}</div>
@@ -179,7 +177,6 @@ const props = defineProps<{
 const emit = defineEmits<{ close: []; saved: [] }>()
 
 const ALL_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS', 'CONNECT', 'TRACE']
-const dbgLog = (...args: any[]) => console.log('[RouteFormModal/dbg]', ...args)
 
 const activeTab = ref('basic')
 const submitting = ref(false)
@@ -211,7 +208,6 @@ function toggleAllMethods() {
 }
 
 async function loadUpstreams(cid: number) {
-  console.log('[RouteFormModal] loadUpstreams called with cid:', cid)
   try {
     const res = await api.get(`/clusters/${cid}/upstreams`, { params: { page_size: 100 } })
     upstreams.value = res.data.items || []
@@ -253,7 +249,6 @@ watch(() => form.cluster_id, (cid) => {
 })
 
 watch(() => props.visible, async (v) => {
-  console.log('[RouteFormModal] visible changed:', v, 'editingRoute:', props.editingRoute, 'copyingRoute:', props.copyingRoute)
   if (!v) return
   formErrors.name = ''
   formErrors.uri = ''
